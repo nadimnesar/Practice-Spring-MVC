bb82ae40506c72f3a0fa81bd6f46363cb2c4ffb9
nadimnesar <nadimnesar.cse@gmail.com>
2024-06-02 19:36:50 +0600

    post-redirect-get

--- src/main/java/com/nadimnesar/controller/MainController.java
+++ src/main/java/com/nadimnesar/controller/MainController.java
@@ -1,8 +1,12 @@
 package com.nadimnesar.controller;
 
+import com.nadimnesar.model.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class MainController {
@@ -11,6 +15,17 @@ public class MainController {
     public ModelAndView getHomePage(ModelAndView modelAndView) {
         modelAndView.setViewName("index");
         modelAndView.addObject("welcome_msg", "Welcome to Spring MVC Project");
+        return modelAndView;
+    }
+
+    @PostMapping("/submit")
+    public ModelAndView postSubmit(ModelAndView modelAndView, @ModelAttribute User user,
+                                   RedirectAttributes redirectAttributes) {
+        System.out.println(user);
+        redirectAttributes.addFlashAttribute("success_msg",
+                "Successfully added a new user info!");
+        modelAndView.setViewName("redirect:/");
+
         return modelAndView;
     }
 }
---
+++ src/main/java/com/nadimnesar/model/User.java
@@ -0,0 +1,40 @@
+package com.nadimnesar.model;
+
+public class User {
+    private String name;
+    private String age;
+    private String email;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", age='" + age + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
